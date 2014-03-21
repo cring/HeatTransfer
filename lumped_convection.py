5d563e9068376a302ab63ba0ccfd1a33560e4814
cring <school.josh@gmail.com>
2014-03-20 23:41:19 -0400

    Added Time Delay and Summation of Temperature Steps for Duhmels Theorm Calculation Function

--- lumped_convection.py
+++ lumped_convection.py
@@ -9,7 +9,6 @@ rhoCopper = 8945 # kg/m^3
 cCopper = 383.1 #J/kg*K
 lCopper = 0.005 #m
 
-
 areaCopper = 3*lCopper
 volumeCopper = lCopper**2
 
@@ -45,8 +44,6 @@ def lumped(tempInitial,tempStep,h,n,t):
 		time.append([i])
 	return temp,time
 
-lumpedBlock = lumped(tempInitial, tempStep, hBlock, nBlock, 100)
-
 def coupledLumped(tempInitial,tempStep,h,t):
 	C1 = h*areaCopper/(rhoCopper*cCopper*volumeCopper)
 	C2 = kAcrylic*lCopper/(sqrt(alphaAcrylic)*rhoCopper*cCopper*volumeCopper)
@@ -54,36 +51,72 @@ def coupledLumped(tempInitial,tempStep,h,t):
 	D1 = C2/2-cmath.sqrt(C2**2-4*C1)/2
 	D2 = C2/2+cmath.sqrt(C2**2-4*C1)/2
 
-	print D1
-	print s.erfc(D1)
-	print e**(D1**2)
-	print C1/((D2-D1)*(D2*D1))
-	print D1*e**(D2**2*1)*s.erfc(D2*sqrt(1))
-	print (D2*e**(D1**2*1)*s.erfc(D1*sqrt(1)))
-	print C1/((D2-D1)*(D2*D1))*((D1*e**(D2**2*1)*s.erfc(D2*sqrt(1)))-(D2*e**(D1**2*1)*s.erfc(D1*sqrt(1)))+D2-D1)
+	temp_t = (tempStep-tempInitial)*C1/((D2-D1)*(D2*D1))*((D1*e**(D2**2*t)*s.erfc(D2*sqrt(t)))-(D2*e**(D1**2*t)*s.erfc(D1*sqrt(t)))+D2-D1)+tempInitial
+	
+	return temp_t.real
+	
+def coupledCalculator(tempInitial,tempStep,h,t):
+	calcTemp=[]
+	calcTime=[]
+	for i in range(t):		
+		calcTemp.append(coupledLumped(tempInitial, tempStep, h, i))
+		calcTime.append([i])
+	return calcTemp,calcTime
 
-	temp = []
-	time= []
+def timeDelayCalculator(tempInitial,tempStep,h,start,stop,step):
+	calcTemp = []
+	calcTime = []
 
-	for i in range(t):
-		temp_t = (tempStep-tempInitial)*C1/((D2-D1)*(D2*D1))*((D1*e**(D2**2*i)*s.erfc(D2*sqrt(i)))-(D2*e**(D1**2*i)*s.erfc(D1*sqrt(i)))+D2-D1)+tempInitial
-		temp.append([temp_t.real])
-		time.append([i])
-	return temp,time
+	for i in arange(0,stop,step):
+		if i < start:
+			calcTemp.append(0)
+			calcTime.append([i])
 
-coupledlumpedBlock = coupledLumped(tempInitial, tempStep, 153.8, 100)
+		else:
+			calcTemp.append(coupledLumped(tempInitial, tempStep, h, i-start))
+			calcTime.append([i])
+	
+	return calcTemp,calcTime
 
-print hBlock
-print coupledlumpedBlock[0][30]
+def timeSumCalculator():
+	timeStepList = [0,0.2,0.4,0.6,0.8,1]
+	tempStepList = [25,43,55,63,65,65]
 
+	summedCalc = zeros(1000).tolist()
+
+	for i in range(len(timeStepList)-1):
+		tempInitial = 0
+		tempStep = tempStepList[i+1]-tempStepList[i]
+		convolution = timeDelayCalculator(tempInitial,tempStep,153.8,timeStepList[i],100,0.1)
+
+		for i in range(len(convolution[0])):
+			summedCalc[i] += convolution[0][i]
+
+	for i in range(len(summedCalc)):
+			summedCalc[i] += tempStepList[0]
+	
+	return summedCalc , convolution[1]
+
+
+lumpedBlock = lumped(tempInitial, tempStep, hBlock, nBlock, 100)
+
+#coupledlumpedBlock2 = coupledCalculator(tempInitial, tempStep, hBlock, 100)
+coupledlumpedBlock2 = coupledCalculator(tempInitial, tempStep, 153.8, 100)
+coupledlumpedBlock = timeSumCalculator()
+
+#print coupledlumpedBlock
+print len(coupledlumpedBlock[0])
+print len(coupledlumpedBlock[1])
 
 plt.xlabel('Time')
 plt.ylabel('Temperature')
 plt.title('Temperature vs. Time of Lumped Block')
 plt.grid(True)
-plt.plot(lumpedBlock[1],lumpedBlock[0])
-plt.plot(coupledlumpedBlock[1],coupledlumpedBlock[0])
+plt.plot(lumpedBlock[1],lumpedBlock[0],label = "Lumped - h~131")
+plt.plot(coupledlumpedBlock[1],coupledlumpedBlock[0], label = "Coupled Lumped - h~154")
+plt.plot(coupledlumpedBlock2[1],coupledlumpedBlock2[0], label = "Uncorrected Coupled Lumped - h~131")
+#plt.legend(loc=4)
 plt.plot([0,100] ,[45,45])
 plt.plot([30,30] ,[0,65])
-plt.axis([0, 100, 0, 65])
+plt.axis([0, 100, 20, 65])
 plt.show()
